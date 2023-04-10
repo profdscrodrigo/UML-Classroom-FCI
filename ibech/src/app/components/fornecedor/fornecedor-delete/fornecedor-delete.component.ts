33dc17cc83da57e6ab3893343ac0e8159ebb7723
BrunoWeezyy <brunovsw@hotmail.com>
2023-04-09 23:06:23 -0300

    fornecedor-list adicionado-variaveis

--- ibech/src/app/components/fornecedor/fornecedor-delete/fornecedor-delete.component.ts
+++ ibech/src/app/components/fornecedor/fornecedor-delete/fornecedor-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Fornecedor } from 'src/app/models/fornecedor';
+import { FornecedorService } from 'src/app/services/fornecedor.service';
 
 @Component({
   selector: 'app-fornecedor-delete',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FornecedorDeleteComponent implements OnInit {
 
-  constructor() { }
+  Fornecedor: Fornecedor = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
+  constructor(
+    private service: FornecedorService,
+    private toast: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.fornecedor.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.fornecedor.id).subscribe(resposta => {
+      resposta.perfis = [];
+      this.fornecedor = resposta;
+    })
+  }
+  
+  delete(): void {
+    this.service.delete(this.fornecedor.id).subscribe(() => {
+      this.toast.success('Técnico deletado com sucesso', 'Delete');
+      this.router.navigate(['fornecedoress'])
+    }, ex => {
+      console.log(ex);
+      if(ex.error.errors){
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+
+    });
   }
 
 }
--- ibech/src/app/components/fornecedor/fornecedor-list/fornecedor-list.component.ts
+++ ibech/src/app/components/fornecedor/fornecedor-list/fornecedor-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Fornecedor } from 'src/app/models/fornecedor';
+import { FornecedorService } from 'src/app/services/fornecedor.service';
 
 @Component({
   selector: 'app-fornecedor-list',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FornecedorListComponent implements OnInit {
 
-  constructor() { }
+  ELEMENT_DATA: Fornecedor[] = [];
+  
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Fornecedor>(this.ELEMENT_DATA);
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private service: FornecedorService
+  ) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
+  findAll() {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Fornecedor>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
+
