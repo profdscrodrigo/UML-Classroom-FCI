2d30096a9e880d24b45c9ecd3252c3af8d6af2e1
BrunoWeezyy <brunovsw@hotmail.com>
2023-04-11 17:25:27 -0300

    cliente-list adicionado - html, css e ts

--- ibech/src/app/components/cliente/cliente-list/cliente-list.component.ts
+++ ibech/src/app/components/cliente/cliente-list/cliente-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/ciente.service';
 
 @Component({
   selector: 'app-cliente-list',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteListComponent implements OnInit {
 
-  constructor() { }
+  ELEMENT_DATA: Cliente[] = [];
+  
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Cliente>(this.ELEMENT_DATA);
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private service: ClienteService
+  ) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
+  findAll() {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Cliente>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
+
