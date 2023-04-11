683a7829ed6bdb561c35b27d2368bfe533710fac
BrunoWeezyy <brunovsw@hotmail.com>
2023-04-11 14:47:25 -0300

    npm install

--- ibech/src/app/components/cliente/cliente-create/cliente-create.component.ts
+++ ibech/src/app/components/cliente/cliente-create/cliente-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/ciente.service';
 
 @Component({
   selector: 'app-cliente-create',
@@ -7,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteCreateComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
+  constructor(
+    private service: ClienteService,
+    private toast: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
+  validaCampos(): boolean {
+    return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid;
+  }
+  
+  addPerfil(perfil: any): void{
+    if (this.cliente.perfis.includes(perfil)){
+      this.cliente.perfis.splice(this.cliente.perfis.indexOf(perfil), 1)
+    } else {
+      this.cliente.perfis.push(perfil);
+    }
+  }
+
+  create(): void {
+    this.service.create(this.cliente).subscribe(() => {
+      this.toast.success('Cliente cadastrado com sucesso', 'Cadastro');
+      this.router.navigate(['clientes'])
+    }, ex => {
+      console.log(ex);
+      if(ex.error.errors){
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+
+    });
+  }
+
 
 }
