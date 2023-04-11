effc4e0fdab62a2a906be28afa951bdff6bf9428
BrunoWeezyy <brunovsw@hotmail.com>
2023-04-11 17:34:39 -0300

    pedidos html, css e ts

--- ibech/src/app/components/pedidos/pedidos.component.ts
+++ ibech/src/app/components/pedidos/pedidos.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { pedidos } from 'src/app/models/pedidos';
 
 @Component({
   selector: 'app-pedidos',
@@ -7,9 +10,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidosComponent implements OnInit {
 
+  ELEMENT_DATA: pedidos[] = [
+    {
+      autor: 'nid',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    }, {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    },
+    {
+      autor: 'nome do autor',
+      titulo: 'O menino do pijama listrado',
+      dataCriacao: '13/09/2023'
+    }
+    
+
+  ]
+
+  displayedColumns: string[] = ['autor', 'titulo', 'acoes'];
+  dataSource = new MatTableDataSource<pedidos>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
